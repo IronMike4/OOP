580d1f1701203f7cdae6eaede9c45366d1a76aba
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-11-04 21:03:50 +0200

    Fields and Properties

---
+++ Fields and Properties/CSharpOOP/CSharpOOP/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpOOP
+{
+    public class Warrior
+    {
+        public int height;
+        public int weight;
+    }
+
+    public class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Warrior theGoodGuy = new Warrior();
+            theGoodGuy.height = 190;
+            theGoodGuy.weight = 80;
+
+            Warrior theBadGuy = new Warrior();
+            theBadGuy.height = 170;
+            theBadGuy.weight = 94;
+
+            Console.WriteLine(theBadGuy.height);
+            Console.WriteLine(theGoodGuy.height);
+        }
+    }
+}
