0c847bbcb89b2d70104977e29e48ea41d5d37355
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-11-04 21:40:43 +0200

    Constructors

---
+++ 04. Constructors/CSharpOOP/CSharpOOP/EntryPoint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSharpOOP
+{
+    public class Warrior
+    {
+        private int height;
+        private int weight;
+        private string name;
+
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public string Name { get; set; }
+
+        public void Greetings(Warrior warrior)
+        {
+            Console.WriteLine($@"Greetings, {warrior.Name}!");    
+        }
+
+        public Warrior(int height, int weight, string name)
+        {
+            Height = height;
+            Weight = weight;
+            Name = name;
+        }
+    }
+
+    public class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Warrior theGoodGuy = new Warrior(190,80,"Batman");
+
+            Warrior theBadGuy = new Warrior(170,94,"Joker");
+            
+            Console.WriteLine(theBadGuy.Height);
+            Console.WriteLine(theGoodGuy.Height);
+
+            theGoodGuy.Greetings(theBadGuy);
+            theBadGuy.Greetings(theGoodGuy);
+        }
+    }
+}
