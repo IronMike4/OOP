4837aecb3661c19dfda6499c08d7ce23375d32cd
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-11-04 21:55:21 +0200

    Namespaces

---
+++ 05. Namespaces/CSharpOOP/CSharpOOP/Characters/Warriors/Warrior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpOOP.Characters.Warriors
+{
+    public class Warrior
+    {
+        private int height;
+        private int weight;
+        private string name;
+
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public string Name { get; set; }
+
+        public void Greetings(Warrior warrior)
+        {
+            Console.WriteLine($@"Greetings, {warrior.Name}!");
+        }
+
+        public Warrior(int height, int weight, string name)
+        {
+            Height = height;
+            Weight = weight;
+            Name = name;
+        }
+    }
+}
