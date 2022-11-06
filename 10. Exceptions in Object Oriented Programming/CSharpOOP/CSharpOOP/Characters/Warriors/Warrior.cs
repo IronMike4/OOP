51e5128fb8bd74b8c2dd175dfe8ed5cbdea0b521
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-11-06 13:13:45 +0200

    Exceptions in Object Oriented Programming

---
+++ 10. Exceptions in Object Oriented Programming/CSharpOOP/CSharpOOP/Characters/Warriors/Warrior.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace CSharpOOP.Characters.Warriors
+{
+    public class Warrior
+    {
+        private int height;
+        private int weight;
+        private int age;
+        private string name;
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                height = value;
+            }
+
+        }
+        public int Weight { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 18 && value <= 45)
+                {
+                    age = value;
+
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, $@"The selected age for {name}, is not a proper age for a Warrior.The age must be between 18 and 45.");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public void Greetings(Warrior warrior)
+        {
+            Console.WriteLine($@"Greetings, {warrior.Name}!");
+        }
+
+        public Warrior(int height, int weight, string name)
+        {
+            Height = height;
+            Weight = weight;
+            Name = name;
+        }
+    }
+}
