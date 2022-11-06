96f531d2d7ecd653ab1d9bb1244816cfda2ab7ea
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-11-06 12:40:44 +0200

    Read, Write and ReadWrite Properties

---
+++ 08. Read Write and ReadWrite Properties/CSharpOOP/CSharpOOP/Characters/Warriors/Warrior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CSharpOOP.Characters.Warriors
+{
+    public class Warrior
+    {
+        private int height;
+        private int weight;
+        private string name;
+
+        public int Height 
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                height = value;
+            }
+
+        }
+        public int Weight { get; set; }
+        public string Name { get; set; }
+
+        public void Greetings(Warrior warrior)
+        {
+            Console.WriteLine($@"Greetings, {warrior.Name}!");
+        }
+
+        public Warrior(int height, int weight, string name)
+        {
+            height = 10;
+            Height = height;
+            Weight = weight;
+            Name = name;
+        }
+    }
+}
