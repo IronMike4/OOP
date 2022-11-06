6689486e5476f61dee6657abf590d012ce438f3e
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-11-06 18:01:00 +0200

    The THIS Keyword, When and Why to Use It

---
+++ 12. The THIS Keyword, When and Why to Use It/CSharpOOP/CSharpOOP/EntryPoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CSharpOOP.Characters.Warriors;
+
+namespace CSharpOOP
+{
+    public class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Warrior theGoodGuy = new Warrior(190,80,"Batman");
+
+            Warrior theBadGuy = new Warrior(170,94,"Joker");
+            
+            Console.WriteLine(theBadGuy.Height);
+            Console.WriteLine(theGoodGuy.Height);
+
+            
+
+            try
+            {
+                theGoodGuy.Age = 25;
+                theBadGuy.Age = 15;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(theGoodGuy.Age);
+            Console.WriteLine(theBadGuy.Age);
+             
+            theGoodGuy.Greetings("Tod");
+        }
+    }
+}
