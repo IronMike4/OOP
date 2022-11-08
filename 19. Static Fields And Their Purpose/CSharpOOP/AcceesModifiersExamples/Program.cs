8dbfb3cac323cc1fd22306403081ebf3115d110b
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-11-08 21:55:59 +0200

    Static Fields And Their Purpose

---
+++ 19. Static Fields And Their Purpose/CSharpOOP/AcceesModifiersExamples/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CSharpOOP.Characters.Warriors;
+
+namespace AcceesModifiersExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior newWarrior = new Warrior();
+        }
+    }
+}
